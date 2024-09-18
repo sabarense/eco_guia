2a03b5525319784e65ec054a1441e8eea52f8ed0
Alexandre Niess <alexandrefersoa@gmail.com>
2024-09-18 16:28:44 -0300

    Criação de telas de exemplo

---
+++ lib/screens/Tela1.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Tela1 extends StatelessWidget {
+  const Tela1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tela 1'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Bem-vindo à Tela 1',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Voltar para a tela anterior (Home)
+              },
+              child: const Text('Voltar para Home'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
