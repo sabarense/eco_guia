8240d3545d84f17999b2e22bb6e15772a8ce92b1
Alexandre Niess <alexandrefersoa@gmail.com>
2024-09-18 16:28:51 -0300

    Criação da tela de Home

---
+++ lib/screens/Home.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'components/BottomNavBar.dart'; // Importa o componente de navegação
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Home Page',
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+      bottomNavigationBar:
+          const BottomNavBar(), // Usa o componente de navegação aqui
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Ação ao clicar no botão central
+        },
+        backgroundColor: Colors.green,
+        child: const Icon(Icons.fullscreen),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
