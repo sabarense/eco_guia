458e1c72f2a804bb976e7ce8de2d057ad450340d
Alexandre Niess <alexandrefersoa@gmail.com>
2024-09-20 13:48:04 -0300

    Definição da cor principal e mudança para a fonte importada

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,20 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple, // Cor semente
+          primary: const Color(0xFF185A46), // Definindo cor primária
+        ),
         useMaterial3: true,
+        // Adicionando a fonte Poppins ao tema
+        fontFamily: 'Poppins', // Define 'Poppins' como a fonte padrão
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+          bodyMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
+          headlineLarge: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          headlineMedium: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+          headlineSmall: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+        ),
       ),
       // Define as rotas da aplicação
       initialRoute: '/',
