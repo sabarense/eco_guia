be77a2663c93672db25c6962367f6b5ef5906eb5
Yan Sabarense <sabarensey@gmail.com>
2024-09-26 18:26:26 -0300

    [FEAT] add login & register routes

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'pages/tabs/tela_1.dart';
 import 'pages/tabs/tela_2.dart';
 import 'pages/tabs/tela_3.dart';
 import 'pages/tabs/tela_4.dart';
+import 'pages/tabs/login.dart';
+import 'pages/tabs/register.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -16,15 +18,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Eco Guia',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.green, // Cor semente
-          primary: const Color(0xFF185A46), // Definindo cor primária
+          seedColor: Colors.green,
+          primary: const Color(0xFF185A46),
         ),
         useMaterial3: true,
-        // Adicionando a fonte Poppins ao tema
-        fontFamily: 'Poppins', // Define 'Poppins' como a fonte padrão
+        fontFamily: 'Poppins',
         textTheme: const TextTheme(
           bodyLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
           bodyMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.normal),
@@ -33,14 +34,15 @@ class MyApp extends StatelessWidget {
           headlineSmall: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
         ),
       ),
-      // Define as rotas da aplicação
-      initialRoute: '/',
+      initialRoute: '/login',
       routes: {
         '/': (context) => const Home(),
         '/location': (context) => const Tela1(),
         '/learn': (context) => const Tela2(),
         '/profile': (context) => const Tela3(),
         '/scan': (context) => const Tela4(),
+        '/login': (context) => const Login(),
+        '/register': (context) => const Register(),
       },
     );
   }
