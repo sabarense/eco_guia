3c284749fce124a7e0f6dbf467e125c4b558d5e7
alexandre-niess <alexandrefersoa@gmail.com>
2024-09-29 18:06:45 -0300

    Import do novo componente do gráfico de pizza

--- lib/pages/rewards/components/historico_tab.dart
+++ lib/pages/rewards/components/historico_tab.dart
@@ -1,20 +1,36 @@
 import 'package:flutter/material.dart';
-import './historico_summary_card.dart'; // Importa o componente criado
+import './historico_summary_card.dart'; // Importa o componente HistoricoSummaryCard
+import './pizza_chart_card.dart'; // Importe o componente PizzaChartCard
 
 class HistoricoTab extends StatelessWidget {
   const HistoricoTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: const [
-            HistoricoSummaryCard(), // Usa o card do Histórico
-          ],
-        ),
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight,
+            ),
+            child: IntrinsicHeight(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0), // Adiciona margem lateral
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    HistoricoSummaryCard(), // Usa o card do Histórico
+                    SizedBox(height: 20), // Espaçamento entre os componentes
+                    PizzaChartCard(), // Usa o card com o gráfico de pizza
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
