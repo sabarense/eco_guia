11ec027f711a36d526ca0ee384c725bc9b725667
Yan Sabarense <sabarensey@gmail.com>
2024-10-30 20:53:17 -0300

    [FEAT] Criação do user model

---
+++ lib/models/user.dart
@@ -0,0 +1,17 @@
+class User {
+  String id;
+  String name;
+  String email;
+  String password;
+
+  User({required this.id, required this.name, required this.email, required this.password});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+    };
+  }
+}
