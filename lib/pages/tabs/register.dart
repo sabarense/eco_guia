e0073910884b5f93647b18072d5f1e0448b5054e
Yan Sabarense <sabarensey@gmail.com>
2024-10-31 20:17:02 -0300

    [FEAT] register page

--- lib/pages/tabs/register.dart
+++ lib/pages/tabs/register.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart'; // Importa o pacote flutter_svg
-import 'package:shared_preferences/shared_preferences.dart'; // Importa o pacote shared_preferences
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:eco_guia/services/user_service.dart';
 class Register extends StatefulWidget {
   const Register({super.key});
 
@@ -19,18 +19,15 @@ class _RegisterState extends State<Register> {
     String email = _emailController.text;
     String password = _passwordController.text;
 
-    // Simulação de registro de usuário
     if (name.isNotEmpty && email.isNotEmpty && password.isNotEmpty) {
-      // Aqui você pode adicionar lógica para salvar o usuário em um banco de dados ou servidor
+      UserService userService = UserService();
+      await userService.addUser(name, email, password);
 
-      // Salvando o status de login no SharedPreferences
       final prefs = await SharedPreferences.getInstance();
       await prefs.setBool('isLoggedIn', true);
 
-      // Se o registro for bem-sucedido, navega para a tela Home
-      Navigator.pushReplacementNamed(context, '/');
+      Navigator.pushReplacementNamed(context, '/login');
     } else {
-      // Exibir erro se as informações forem inválidas
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
@@ -38,9 +35,7 @@ class _RegisterState extends State<Register> {
           content: const Text('Preencha todos os campos!'),
           actions: [
             TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
+              onPressed: () => Navigator.pop(context),
               child: const Text('OK'),
             ),
           ],
@@ -52,76 +47,108 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); 
+          },
+        ),
         title: const Text('Crie sua Conta'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF185A46), Color(0xFF0B3D30)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            // Adicionando o título acima da logo
-            const Text(
-              'Eco Guia',
-              style: TextStyle(
-                fontSize: 24, // Tamanho da fonte do título
-                fontWeight: FontWeight.bold, // Estilo em negrito
-              ),
-            ),
-            const SizedBox(height: 16), // Espaçamento entre o título e a logo
-            // Adicionando a logo no topo
-            SvgPicture.asset(
-              'assets/logo.svg',
-              height: 150, // Defina o tamanho da logo
-            ),
-            const SizedBox(height: 70), // Espaçamento entre a logo e os campos
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                labelText: 'Nome',
-                labelStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
-                border: const OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+            Expanded(
+              child: Center(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const Text(
+                        'Eco Guia',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      SvgPicture.asset(
+                        'assets/logo.svg',
+                        height: 150,
+                      ),
+                      const SizedBox(height: 40),
+                      TextField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          labelText: 'Nome',
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: const OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'E-mail',
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: const OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: const OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
+                      const SizedBox(height: 24),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          foregroundColor: Color(0xFF185A46),
+                        ),
+                        onPressed: _register,
+                        child: const Text('Registrar'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                labelStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
-                border: const OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                '© 2024 Eco Guia. Todos os direitos reservados.',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 12,
                 ),
               ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Senha',
-                labelStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
-                border: const OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
-                ),
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).colorScheme.primary,
-                foregroundColor: Colors.white,
-              ),
-              onPressed: _register,
-              child: const Text('Registrar'),
             ),
           ],
         ),
